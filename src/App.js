6813226fa9bc853f56014ee5806af9c93ffdbf32
harshdev92 <haggarwal25@gmail.com>
2020-02-06 12:40:39 +0530

    added checkout page,checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import HomePage from "./pages/homepages/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+
 class App extends React.Component {
   constructor() {
     super();
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
